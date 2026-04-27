9fddc32d46a1adb41f893fac1bba5abe4b391ad6
LuisSaldarriaga <u20242226970@usco.edu.co>
2026-04-27 17:59:20 -0500

    Corrección de detección de endpoints en Swagger

--- parqueadero/src/main/java/com/parqueadero/parqueadero/controller/VehiculoController.java
+++ parqueadero/src/main/java/com/parqueadero/parqueadero/controller/VehiculoController.java
@@ -1,15 +1,21 @@
 package com.parqueadero.parqueadero.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.parqueadero.parqueadero.model.Vehiculo;
 import com.parqueadero.parqueadero.repository.VehiculoRepository;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/vehiculos")
+@CrossOrigin
 public class VehiculoController {
 
     @Autowired
